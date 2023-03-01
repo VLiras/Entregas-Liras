385f18e3036453e94267d38920ba7d217d033a7f
VLiras <valentinliras@gmail.com>
2023-03-01 12:08:46 -0300

    Contenido de Clase 13 lleno de errores

--- src/Components/CartContainer/CartContainer.jsx
+++ src/Components/CartContainer/CartContainer.jsx
@@ -1,9 +1,47 @@
+import { addDoc, collection, doc, getFirestore, updateDoc } from "firebase/firestore"
+import { useState } from "react"
 import { useCartContext } from "../../Context/CartContext"
 import products from "../../Utils/products"
 import Total from "../Total/Total"
 const CartContainer = () => {
     const {cartList,cleanCart} = useCartContext()
     // => Listado de productos
+    const [dataForm,setDataForm] = useState({
+        name:'',
+        phone:'',
+        email:''
+    })
+    const totalPrice = () => {
+        cartList.map((price,cant) => price = price*cant)
+    }
+    const createOrder = (event) => {
+        event.preventDefault()
+        const order = {}
+        order.buyer = dataForm
+        order.item = cartList.map(({id,name,price}) => ({id,name,price})),    
+        order.total = totalPrice()
+        
+        const db = getFirestore()
+        const queryColection = collection(db,'orders')
+        addDoc(queryColection)
+        .then(ans => console.log(ans))
+        .catch(err => console.log(err))
+        .finally(()=>{})
+
+        //Actualizar un documento en firestore
+        const queryDoc = doc(db,'Productos','7wAjigtpnwKfy8pT8wWD')
+        updateDoc(queryDoc, {
+            stock:80
+        })
+        .then(() => console.log('Producto Actualizado'))
+        .catch(err => console.error(err))
+    }
+    const handleOnChange = (e) => {
+        setDataForm({
+            ...dataForm,
+            [e.target.name]:e.target.value
+        })
+    }                                    
     return(
         <div className="cartContainer w-100 rounded-4 p-0">
             <div style={{width:'200%'}}>
@@ -33,6 +71,12 @@ const CartContainer = () => {
             }
             <div style={{width:'200%',height:'4rem'}} className="border center p-3 mt-3">
                 <button type="button" className="btn btn-danger rounded-pill" onClick={cleanCart}>Vaciar Carrito</button>
+                <button type="button" onClick={() => createOrder()} className="btn btn-primary rounded-pill">Generar Orden</button>
+            </div>
+            <div>
+                <form>
+                    {/* Crear Formulario */}
+                </form>
             </div>
         </div>
     )
