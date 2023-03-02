9a6ed90d4501ee2f171e7af34533b1532446be08
VLiras <valentinliras@gmail.com>
2023-03-02 18:30:47 -0300

    Con detalle

--- src/Components/ItemCount/ItemCount.jsx
+++ src/Components/ItemCount/ItemCount.jsx
@@ -16,9 +16,9 @@ const ItemCount=({initial,stock, onAdd})=>{
     return(
         <div className="rounded-3 border mt-3 mb-3 p-3">
             <div style={{margin:'0 auto'}} className="cant row w-100 mt-2">
-                <button style={{fontSize:'2vw'}} onClick={() => {add()}} type="button" className="btn btn-outline-light col-3">+</button>
+                <button style={{fontSize:'2vw'}} onClick={() => {add()}} type="button" className="btn btn-light col-3">+</button>
                 <article className="stock col-6"><h3>{count}</h3></article>
-                <button style={{fontSize:'2vw'}} onClick={() => {rest()}} type="button" className="btn btn-outline-light col-3">-</button>
+                <button style={{fontSize:'2vw'}} onClick={() => {rest()}} type="button" className="btn btn-light col-3">-</button>
             </div>
             <div className="w-100 mt-3">
                 <button className="btn btn-danger w-100 rounded-pill mt-2 mb-2">Comprar</button>
--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -5,6 +5,7 @@ import gProducts  from '../../Utils/gProducts'
 import products from '../../Utils/products'
 import { useCartContext } from '../../Context/CartContext'
 import Toast from 'react-bootstrap/Toast'
+import { doc, getDoc, getFirestore } from 'firebase/firestore'
 const ItemDetail = () => {
     const [product,setProduct]=useState({})
     const {idProduct} = useParams()
@@ -13,34 +14,36 @@ const ItemDetail = () => {
     // const [isCount,setIsCount] = useState(true) => Clase 11
         
     useEffect(() => {
-        gProducts(idProduct)
-        .then(answer => setProduct(answer))
-        .catch(err => setProduct(err))
-               
+        const db = getFirestore()
+        const queryProd = doc(db,'Products', idProduct)
+        getDoc(queryProd)
+        .then(ans => setProduct({id: ans.id,...ans.data()}))
+        .catch(err => console.error(err))
+        .finally(() => setLoading(false))
     },[])
     // console.log("Cartlist:" ,cartList)
     const onAdd = (cant) => {
         console.log(cant)
         addToCart({ ...product, cant })
     }
-    console.warn(product) 
+     
     return(
             <div style={{margin:'0 auto'}} className="detailBlock row rounded-4 w-100">
                 <div className="images col-6">
                     <div>
-                        <div className='p-3'><img className="w-100 h-100 rounded-4" src="#" alt="image" /></div>
+                        <div className='p-3'><img className="w-100 h-100 rounded-4" src={product.photo} alt="image" /></div>
                     </div>
                 </div>
                 <div className="col-6">
                     <div className='details m-3 p-3 rounded-4'>
                         <div>
-                            <h2>Marca + Modelo</h2>
-                            <h3 className='price'>Precio</h3>
-                            <p style={{fontSize:'1.5rem'}} className='stock'>Disponibles: Stock Unidades</p>
+                            <h2>{product.make} {product.model}</h2>
+                            <h3 className='price'>U$S {product.price}</h3>
+                            <p style={{fontSize:'1.5rem'}} className='stock'>Disponibles: {product.stock} Unidades</p>
                         </div>
                         <br />
                         <div>
-                            <ItemCount initial={1} stock={10} onAdd={onAdd} />                             
+                            <ItemCount initial={1} stock={product.stock} onAdd={onAdd} />                             
                             {/* {isCount ? <ItemCount initial={1} stock={10} onAdd={onAdd} />
                              : <Link to="/cart"><button className='btn btn-primary'>Ir al Carrito</button></Link>
                             } */}
