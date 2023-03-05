66961b1b6c242dd539ab0a99708f5189951666ad
VLiras <valentinliras@gmail.com>
2023-03-05 12:32:04 -0300

    Mejoras en los botones

--- src/Components/ItemCount/ItemCount.jsx
+++ src/Components/ItemCount/ItemCount.jsx
@@ -1,7 +1,7 @@
 import { useState,useContext } from "react";
 import products from "../../Utils/products";
 
-const ItemCount=({initial,stock, onAdd})=>{
+const ItemCount = ({initial,stock, onAdd}) => {
     const [count,setCount]=useState(initial)
     const add = (props) => {
         if (count < stock) {
@@ -14,11 +14,19 @@ const ItemCount=({initial,stock, onAdd})=>{
         }
     }
     return(
-        <div className="rounded-3 border mt-3 mb-3 p-3">
+        <div className="countContainer rounded-4 mt-3 mb-3 p-3">
             <div style={{margin:'0 auto'}} className="cant row w-100 mt-2">
-                <button style={{fontSize:'2vw'}} onClick={() => {add()}} type="button" className="btn btn-light col-3">+</button>
+                <div className="col-3">
+                    <article className="buttonAligners center">
+                        <button style={{fontSize:'2vw'}} onClick={() => {add()}} type="button" className="btn btn-outline-light rounded-circle w-100">+</button>
+                    </article>
+                </div>
                 <article className="stock col-6"><h3>{count}</h3></article>
-                <button style={{fontSize:'2vw'}} onClick={() => {rest()}} type="button" className="btn btn-light col-3">-</button>
+                <div className="col-3">
+                    <article className="buttonAligners center">
+                        <button style={{fontSize:'2vw'}} onClick={() => {rest()}} type="button" className="btn btn-outline-light rounded-circle w-100">-</button>
+                    </article>
+                </div>
             </div>
             <div className="w-100 mt-3">
                 <button className="btn btn-danger w-100 rounded-pill mt-2 mb-2">Comprar</button>
