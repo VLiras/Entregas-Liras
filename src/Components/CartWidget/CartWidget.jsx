c2378b3da72322ffa651ef8357c9274cd74b27ef
VLiras <valentinliras@gmail.com>
2023-03-16 18:16:01 -0300

    CartCounter funcionando

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -3,13 +3,12 @@ import { useCartContext } from '../../Context/CartContext'
 
 function Cart(){
     const {cartList} = useCartContext()
-     
-    
+    const cartCounter = () => cartList.reduce((count,product) => count += (product.cant * 1),0 )
     return(
         <div className="cart text-light w-50">
             <i style={{fontSize:'2vw'}} className="fa-solid fa-cart-shopping p-1"/>
-            <label style={{color:'crimson'}}>{cartList.length}</label>
-            {/* <label style={{color:'crimson'}}>{cartElem()}</label> */}
+            {/* <label style={{color:'crimson'}}>{cartList.length}</label> */}
+            <label style={{color:'crimson'}}>{cartCounter()}</label>
         </div>
     )
 }
